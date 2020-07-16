4b4d5ccdce3d04379ad9f91a902e808ebb03c1cb
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-07-16 20:30:42 +0200

    Flush output in scrape-kleinanzeigen more modern

--- scrape-kleinanzeigen/scrape-kleinanzeigen.py
+++ scrape-kleinanzeigen/scrape-kleinanzeigen.py
@@ -2,7 +2,6 @@
 """Run predefined search queries against ebay kleinanzeigen in a loop
 and notify per pushover in case a new match comes up."""
 import io
-import sys
 import time
 from typing import Set
 
@@ -42,8 +41,7 @@ def heartbeat():
     global last_hour
     hour = time.localtime().tm_hour
     if 'last_hour' not in globals() or hour != last_hour:
-        print(f"{time.asctime()} heartbeat")
-        sys.stdout.flush()
+        print(f"{time.asctime()} heartbeat", flush=True)
     last_hour = hour
 
 
